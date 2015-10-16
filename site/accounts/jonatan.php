24a62cb4793cc268edd38c86f08b9af0399b1e4d
Jonatan Eriksson <jonatan@projectprojects.com>
2015-10-16 10:43:53 -0400

    Working copy before debugging and panel
    Former-commit-id: b248b49b01a2171b040977383cce29bc964aa3d2

---
+++ site/accounts/jonatan.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: jonatan
+email: jonatan@tsto.org
+password: >
+  $2a$10$PB7V7jNO37mIWl.ZHvq4NOeDoJWHb/61o4YIrMmm7HxvMtHydyjDu
+language: en
+role: admin
