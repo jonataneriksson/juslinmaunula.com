1d15f642941fdce4eb41b734d111a1948fe2ce05
Jonatan Eriksson <jonatan@projectprojects.com>
2015-11-10 10:36:53 -0500

    Added google analytics

--- site/templates/default.php
+++ site/templates/default.php
@@ -23,6 +23,14 @@
         <script src="../../assets/components/angular-sanitize/angular-sanitize.js"></script>
         <script src="../../assets/components/angular-ui-router/release/angular-ui-router.js"></script>
         <!-- endbower -->
-        <script src="assets/app.js"></script>        
+        <script src="assets/app.js"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-53633146-2', 'auto');
+            ga('send', 'pageview');
+        </script>     
     </body>
 </html>
