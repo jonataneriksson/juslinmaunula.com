b24bdd943c8637aa2ebe89a09abba2c1373b26bd
Jonatan Eriksson <jonatan@projectprojects.com>
2015-11-19 12:43:10 -0500

    Optimizing speed

--- site/templates/default.php
+++ site/templates/default.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <base href="/">    
     <!-- bower:css -->
-    <link rel="stylesheet" href="../../assets/components/normalize.css/normalize.css" />
-    <link rel="stylesheet" href="../../assets/components/animate-css/animate.css" />
+    <link rel="stylesheet" href="../../assets/components/normalize.css/normalize.min.css" />
+    <!--<link rel="stylesheet" href="../../assets/components/animate-css/animate.css" />-->
     <!-- endbower -->
     <link rel="stylesheet" href="assets/stylesheets.css" />    
     </head>
@@ -18,14 +18,12 @@
            
         <!-- bower:js -->
         <script src="../../assets/components/angular/angular.min.js"></script>
-        <script src="../../assets/components/angular-ui-router/release/angular-ui-router.js"></script>
+        <script src="../../assets/components/angular-ui-router/release/angular-ui-router.min.js"></script>
         <script src="../../assets/components/angular-sanitize/angular-sanitize.min.js"></script>          
-        <script src="../../assets/components/picturefill/dist/picturefill.js"></script>        
-        <script src="../../assets/components/angular-picture/src/angular-picture.js"></script>
         <script src="../../assets/components/angular-columnify/angular-columnify.js"></script>        
         <!-- endbower -->
         <script src="assets/app.js"></script>
-        <script>
+        <script defer>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
