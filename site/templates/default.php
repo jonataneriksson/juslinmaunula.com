78180f6911423caf76ed495596c24ace1bd49e9b
Jonatan Eriksson <jonatan@projectprojects.com>
2015-11-20 13:26:43 -0500

    IE debugging

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,6 +4,7 @@
     <title>Juslin Maunula</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <base href="/">    
     <!-- bower:css -->
     <link rel="stylesheet" href="../../assets/components/normalize.css/normalize.min.css" />
